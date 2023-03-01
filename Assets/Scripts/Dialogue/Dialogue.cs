ef9e9e0b4342f134f33b5b4691a6acfdb198c94e
Amaan Shawkath <amaanshaw@live.co.uk>
2023-03-01 21:42:21 +0000

    Setting up dialogue scritable object and folders

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nanoreno.Dialogue
+{
+    [CreateAssetMenu(fileName = "New Dialogue", menuName = "Nanoreno/Dialogue", order = 0)]
+    public class Dialogue : ScriptableObject
+    {
+        [SerializeField]
+        List<DialogueNode> nodes = new List<DialogueNode>();
+
+#if UNITY_EDITOR
+        private void Awake()
+        {
+            if (nodes.Count == 0)
+            {
+                nodes.Add(new DialogueNode());
+            }
+        }
+#endif
+
+        public IEnumerable<DialogueNode> GetAllNodes()
+        {
+            return nodes;
+        }
+    }
+}
